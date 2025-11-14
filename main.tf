3110b506ebd33d18b0dbd283172918ca66ed64cd
Tehedor <sergiot1805@gmail.com>
2025-11-14 16:05:12 +0100

    fix: Refactor firewall module port configuration to simplify port handling

--- main.tf
+++ main.tf
@@ -376,10 +376,15 @@ module "firewall" {
   ingress_firewall_policy_id = "ingress_policy"
   egress_firewall_policy_id  = "egress_policy"
 
-ports = compact(flatten([
-  [module.admin_vm.port_id],        # Convertimos el string a lista (1 elemento)
-  module.loadbalancer.lb_port_id    # Ya es lista → OK
-]))
+# ports = compact(flatten([
+#   [module.admin_vm.port_id],        # Convertimos el string a lista (1 elemento)
+#   module.loadbalancer.lb_port_id    # Ya es lista → OK
+# ]))
+
+  ports = compact([
+    module.admin_vm.port_id,
+    # module.loadbalancer.lb_port_id
+  ])
 
   depends_on = [
     module.router,
