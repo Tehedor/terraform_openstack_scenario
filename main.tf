2211fad673cfd4ba6ee218bfe09311f51a3b2cff
Tehedor <sergiot1805@gmail.com>
2025-11-14 15:53:43 +0100

    fix: Improve port configuration in firewall module by ensuring proper list formatting

--- main.tf
+++ main.tf
@@ -376,10 +376,10 @@ module "firewall" {
   ingress_firewall_policy_id = "ingress_policy"
   egress_firewall_policy_id  = "egress_policy"
 
-  ports = compact(
-    module.admin_vm.port_id,
-    flatten([module.loadbalancer.lb_port_id]),
-  )
+  ports = compact(flatten([
+    [module.admin_vm.port_id],   # convierte el string en lista con 1 elemento
+    module.loadbalancer.lb_port_id
+  ]))
 
   depends_on = [
     module.router,
